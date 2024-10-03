9e5970439cbdd2774352f0f75cc5533f6848c766
Ali Hussain <alihussainpid@gmail.com>
2024-10-03 05:17:59 +0500

    Final Firebase private key update

--- configurations/FirebaseBaseAdminConfig.js
+++ configurations/FirebaseBaseAdminConfig.js
@@ -1,10 +1,9 @@
 
-
 const firebaseAdminConfig = {
     "type": process.env.FIRSEBASE_TYPE,
     "project_id": process.env.FIRSEBASE_PROJECT_ID,
     "private_key_id": process.env.FIREBASE_PRIVATE_KEY_ID,
-    "private_key": process.env.FIREBASE_PRIVATE_KEY,
+    "private_key": process.env.FIREBASE_PRIVATE_KEY.replaceAll("\\n", "\n"),
     "client_email": process.env.FIREBASE_CLIENT_EMAIL,
     "client_id": process.env.FIREBASE_CLIENT_ID,
     "auth_uri": process.env.FIREBASE_AUTH_URI,
