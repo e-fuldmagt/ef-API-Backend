5961e8f7e167bdf9159abeece3f4728d743840d3
Ali Hussain <alihussainpid@gmail.com>
2024-10-05 17:51:00 +0500

    Changing ENVIRONMENT VARIABLES

--- configurations/FirebaseServiceAccountKey.js
+++ configurations/FirebaseServiceAccountKey.js
@@ -5,13 +5,13 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const firebaseConfig = {
-  apiKey: process.env.apiKey,
-  authDomain: process.env.authDomain,
-  projectId: process.env.projectId,
-  storageBucket: process.env.storageBucket,
-  messagingSenderId: process.env.messagingSenderId,
-  appId: process.env.appId,
-  measurementId: process.env.measurementId,
+  apiKey: process.env.APIKEY,
+  authDomain: process.env.AUTHDOMAIN,
+  projectId: process.env.PROJECTID,
+  storageBucket: process.env.STORAGEBUCKET,
+  messagingSenderId: process.env.MESSAGINGSENDERID,
+  appId: process.env.APPID,
+  measurementId: process.env.MEASUREMENTID,
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
