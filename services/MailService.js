2fb5ca3e2008283c247fd31159a4dda39960b94f
Ali Hussain <alihussainpid@gmail.com>
2024-10-06 18:15:51 +0500

    Mailing Address Changed

--- services/MailService.js
+++ services/MailService.js
@@ -2,20 +2,33 @@ const nodemailer = require('nodemailer');
 
 async function sendEmail(email, subject, body) {
     var mail = nodemailer.createTransport({
-        service: 'gmail',
+        host: 'smtp.simply.com',
+        port: 587,
+        secure: false,
         auth: {
             user: process.env.EMAIL_ID, // Your email id
             pass: process.env.EMAIL_PASSWORD // Your password
-        }
+        },
+        tls: {
+            rejectUnauthorized: false,
+        },
     });
     var mailOptions = {
-        from: "Power of Attorney",
+        from: process.env.EMAIL_ID,
         to: email,
         subject: subject,
         html: body
     };
- 
-    return mail.sendMail(mailOptions)
-}
 
+    let response = null;
+
+    try{
+        response = await mail.sendMail(mailOptions);
+    }
+    catch(err){
+        console.log(err)
+    }
+ 
+    return response
+}
 module.exports = {sendEmail};
