244b15a36a96b2192cfd9e8d9a31d3434509b026
Ali Hussain <alihussainpid@gmail.com>
2024-10-06 17:56:47 +0500

    changing back the settings for mail sending

--- services/MailService.js
+++ services/MailService.js
@@ -2,34 +2,20 @@ const nodemailer = require('nodemailer');
 
 async function sendEmail(email, subject, body) {
     var mail = nodemailer.createTransport({
-        host: 'websmtp.simply.com',
-        port: 587,
-        secure: false,
+        service: 'gmail',
         auth: {
-            user: "noreply@e-fuldmagt.dk", // Your email id
-            pass: "bismillah@1@Ahad" // Your password
-        },
-        tls: {
-            rejectUnauthorized: false,
-        },
+            user: process.env.EMAIL_ID, // Your email id
+            pass: process.env.EMAIL_PASSWORD // Your password
+        }
     });
     var mailOptions = {
-        from: "noreply@e-fuldmagt.dk",
+        from: "Power of Attorney",
         to: email,
         subject: subject,
         html: body
     };
-
-    let response = null;
-
-    try{
-        response = await mail.sendMail(mailOptions);
-    }
-    catch(err){
-        console.log(err)
-    }
  
-    return response
+    return mail.sendMail(mailOptions)
 }
 
 module.exports = {sendEmail};
